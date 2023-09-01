6ddd408c41cb8d3954fa45e0b00b0236fe98a130
Your Name <you@example.com>
2023-09-01 09:43:58 -0700

    update keymap for telescope

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -12,7 +12,7 @@ return {
 			local wk = require("which-key")
 			wk.register({
 				f = {
-					name = "file", -- group name
+					name = "find", -- group name
 				},
 				}, { prefix = "<leader>" })
 		end,
@@ -58,8 +58,10 @@ return {
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
 			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "find"})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "grep"})
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "find file"})
+			vim.keymap.set('n', '<leader>fp', builtin.git_files, {desc = "find project (git) file"})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "generic grep"})
+			vim.keymap.set('n', '<leader>fs', builtin.grep_string, {desc = "grep selected string"})
 			vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "buffers"})
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "help_tags"})
 		end
