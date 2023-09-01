0d81bc23276e37fab8f78bb8e76e8459d5a75b17
Your Name <you@example.com>
2023-08-31 17:06:06 -0700

    require that some LSPs get installed by default

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -25,6 +25,10 @@ return {
 				})
 			end)
 
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "jedi_language_server", },
+			})
+
 			-- (Optional) Configure lua language server for neovim
 			require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
