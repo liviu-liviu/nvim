770283f0100cf0d5a76375980701a0c3ce8fffc7
Your Name <you@example.com>
2023-09-04 20:16:50 -0700

    add INCOMPLETE file path formatting

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -57,6 +57,19 @@ return {
 		-- or branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
+			-- require('telescope').setup{
+			-- 	defaults = {
+			-- 		path_display = function(opts, path)
+			-- 			local limit = 25 -- How to find the limit at runtime?
+			-- 			local space_for_location_in_file = 3
+			-- 			local file = string.sub(path, -(limit - space_for_location_in_file))
+			--
+			-- 			return string.format("%s", file)
+			--
+			-- 		end,
+			-- 	}
+			-- }
+
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "find file"})
 			vim.keymap.set('n', '<leader>fp', builtin.git_files, {desc = "find project (git) file"})
