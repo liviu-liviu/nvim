3ffc492e2aacc5dc4791564617629af4b34bf019
Your Name <you@example.com>
2023-09-07 07:27:40 -0700

    add new key maps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,10 @@ map.set("n", "<leader>x", "<cmd>!chmod u+x %<CR>", {silent = true, desc = "make 
 
 map.set("n", "<leader>o", 'o<Esc>0"_D', {desc = "insert empty line below"})
 map.set("n", "<leader>O", 'O<Esc>0"_D', {desc = "insert empty line above"})
+
+map.set("n", "<leader>h", "<cmd>set hlsearch!<CR>", {desc = "toggle hlsearch"})
+
+map.set({"n", "v"}, "<leader>y", '"+y', {noremap = true, desc = "yank into PRIMARY"})
+map.set({"n", "v"}, "<leader>p", '"+p', {noremap = true, desc = "past from PRIMARY"})
+map.set({"n", "v"}, "<leader>Y", '"*y', {noremap = true, desc = "yank into CLIPBOARD"})
+map.set({"n", "v"}, "<leader>P", '"*p', {noremap = true, desc = "past from CLIPBOARD"})
