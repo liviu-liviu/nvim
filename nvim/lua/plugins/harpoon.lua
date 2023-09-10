ef73bc492e288cb3136b011375d057ec4d51fbba
Your Name <you@example.com>
2023-09-10 06:39:43 -0700

    change harpoon keymap C-e and C-y is for scrolling

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
 			local ui = require("harpoon.ui")
 
 			vim.keymap.set("n", "<leader>a", mark.add_file, {desc = "add file to harpoon"})
-			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+			vim.keymap.set("n", "<C-a>", ui.toggle_quick_menu)
 
 			vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
 			vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
